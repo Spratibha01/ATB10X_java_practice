c79a615126ccc0641f475bfec043d6907be02c61
prati <pratisat1823@gmail.com>
2025-01-07 22:49:42 +0530

    1st

---
+++ src/execution_java_basic/L001_GoodMorningPratibha.java
@@ -0,0 +1,7 @@
+package execution_java_basic;
+
+public class L001_GoodMorningPratibha {
+    public static void main(String[] args) {
+        System.out.println("Good Morning Pratibha");
+    }
+}
