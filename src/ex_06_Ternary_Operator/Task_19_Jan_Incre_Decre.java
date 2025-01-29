528cbc93f168015b78f066885874c3b489f314c1
prati <pratisat1823@gmail.com>
2025-01-29 22:20:49 +0530

    task 19 jan

---
+++ src/ex_06_Ternary_Operator/Task_19_Jan_Incre_Decre.java
@@ -0,0 +1,9 @@
+package ex_06_Ternary_Operator;
+
+public class Task_19_Jan_Incre_Decre {
+    public static void main(String[] args) {
+        int a=10;
+        System.out.println( ++a + a++ +a++);
+        System.out.println(a);
+    }
+}
