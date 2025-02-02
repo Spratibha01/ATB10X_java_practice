f871b6d7ef2932205a98db0debe04c4d1a185b6b
prati <pratisat1823@gmail.com>
2025-02-02 10:50:20 +0530

    task 19 jan

---
+++ src/ex_06_Ternary_Operator/Task_19_Jan_02_Incre_Decre.java
@@ -0,0 +1,11 @@
+package ex_06_Ternary_Operator;
+
+public class Task_19_Jan_02_Incre_Decre {
+    public static void main(String[] args) {
+
+        int a=20;
+        System.out.println(--a + a++ + a--);
+        System.out.println(a);
+
+    }
+}
---
+++ src/ex_06_Ternary_Operator/Task_Write_9th_table.java
@@ -0,0 +1,29 @@
+package ex_06_Ternary_Operator;
+
+public class Task_Write_9th_table {
+    public static void main(String[] args) {
+        int n= Integer.parseInt (args[0]);
+        //System.out.println( "9 x 1= %d\n", number*1);
+        System.out.printf("%dx1=%d\n",n,n*1);
+        System.out.printf("%dx2=%d\n",n,n*2);
+        System.out.printf("%dx3=%d",n,n*3);
+        System.out.println();
+        System.out.printf("%dx4=%d",n,n*4).println();
+        System.out.printf("%dx5=%d",n,n*5).println();;
+        System.out.printf("%dx6=%d",n,n*6).println();;
+        System.out.printf("%dx7=%d",n,n*7);
+        System.out.println();
+        System.out.printf("%dx8=%d",n,n*8);
+        System.out.println();
+        System.out.printf("%dx9=%d",n,n*9);
+
+
+
+
+
+
+
+
+
+    }
+}
