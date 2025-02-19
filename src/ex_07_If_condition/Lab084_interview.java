16705cdfad4746f9ccfe8d64983b5d045378efab
prati <pratisat1823@gmail.com>
2025-02-19 12:01:03 +0530

    task 19 jan_2

---
+++ src/ex_07_If_condition/Lab084_interview.java
@@ -0,0 +1,13 @@
+package ex_07_If_condition;
+
+public class Lab084_interview {
+    public static void main(String[] args) {
+        boolean a = !true;
+        if (a){
+            System.out.println("a is true!");
+        }
+        else {
+            System.out.println("a is false");
+        }
+    }
+}
